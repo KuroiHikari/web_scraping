bc24d6b561d5e3d990ef7104fd34fc1a22a069e1
KuroiHikari <kuroihikari7@gmail.com>
2023-12-19 14:46:46 +0100

    updated python for database

--- demo.py
+++ demo.py
@@ -3,143 +3,22 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 import numpy as np
-
-## HTTP Request
-## store website in variable
-# website = 'https://www.cars.com/shopping/results/?stock_type=cpo&makes%5B%5D=mercedes_benz&models%5B%5D=&list_price_max=&maximum_distance=20&zip='
-# print("Website:", website)
-#
-# # Get Request
-# response = requests.get(website)
-#
-# # Status Code
-# print("Status code:", response.status_code)
-#
-# # Soup Object
-# soup = BeautifulSoup(response.content, 'html.parser')
-#
-# # Results
-# results = soup.find_all('div', {'class': 'vehicle-card'})
-#
-# # Length
-# print("Length:", len(results))
-
-# Target necessary data
-# Name
-# Mileage
-# Dealer Name
-# Rating
-# Rating Count
-# Price
+import re
 
 
-# # Name
-# name = results[0].find('h2')
-# if name:
-#     print("Name:", name.get_text())
-# else:
-#     print("Name: not available")
-#
-# # Mileage
-# mileage = results[0].find('div', {'class': 'mileage'})
-# if mileage:
-#     print("Mileage:", mileage.get_text())
-# else:
-#     print("Mileage: not available")
-#
-# # Rating
-# rating = results[0].find('span', {'class': 'sds-rating__count'})
-# if rating:
-#     print("Rating:", rating.get_text())
-# else:
-#     print("Rating: not available")
-#
-# # Review count
-# review = results[0].find('span', {'class': 'sds-rating__link'})
-# if review:
-#     print("Reviews:", review.get_text())
-# else:
-#     print("Reviews: not available")
-#
-# # Price
-# price = results[0].find('span', {'class': 'primary-price'})
-# if price:
-#     print("Price:", price.get_text())
-# else:
-#     print("Price: not available")
-#
-# # Dealer
-# dealer = results[0].find('div', {'class': 'dealer-name'})
-# if dealer:
-#     print("Dealer:", dealer.get_text().strip())
-# else:
-#     print("Dealer: not available")
+# Function to extract year, brand, and type from the name
+def extract_info(name):
+    match = re.match(r'(\d{4})\s(.+?)\s(.+)', name)
+
+    if match:
+        year = match.group(1)
+        brand = match.group(2)
+        car_type = match.group(3)
+        return year, brand, car_type
+    else:
+        return 'n/a', 'n/a', 'n/a'
 
 
-
-# # For Loop for all cars in 1 page
-# name = []
-# mileage = []
-# dealer = []
-# rating = []
-# reviews = []
-# price = []
-#
-# for result in results[1:]:
-#     #name
-#     try:
-#         name.append(result.find('h2').get_text())
-#     except:
-#         name.append('n/a')
-#
-#     #mileage
-#     try:
-#         mileage.append(result.find('div', {'class': 'mileage'}).get_text())
-#     except:
-#         mileage.append('n/a')
-#
-#     #dealer
-#     try:
-#         dealer.append(result.find('div', {'class': 'dealer-name'}).get_text().strip())
-#     except:
-#         dealer.append('n/a')
-#
-#     #rating
-#     try:
-#         rating.append(result.find('span', {'class': 'sds-rating__count'}).get_text())
-#     except:
-#         rating.append('n/a')
-#
-#     #reviews
-#     try:
-#         reviews.append(result.find('span', {'class': 'sds-rating__link'}).get_text())
-#     except:
-#         reviews.append('n/a')
-#
-#     #price
-#     try:
-#         price.append(result.find('span', {'class': 'primary-price'}).get_text())
-#     except:
-#         price.append('n/a')
-
-# Print out results
-# print(name)
-# print(price)
-# print(mileage)
-# print(rating)
-# print(reviews)
-# print(dealer)
-
-# # Panda Dataframe
-# car_dealer = pd.DataFrame({'Name': name, 'Price': price, 'Mileage': mileage, 'Rating': rating, 'Reviews': reviews, 'Dealer': dealer})
-# print(car_dealer)
-#
-# # Data cleaning
-# car_dealer['Reviews'] = car_dealer['Reviews'].apply(lambda x: x.strip('reviews)').strip('('))
-# car_dealer['Mileage'] = car_dealer['Mileage'].apply(lambda x: x.strip('mi.'))
-#
-# # Output in Excel
-# car_dealer.to_excel('single_page_car.xlsx', index=False)
 
 # Pagination
 name = []
@@ -151,8 +30,8 @@ price = []
 
 for i in range(1,11):
     # Website in variable
-    website = 'https://www.cars.com/shopping/results/?makes[]=mercedes_benz&maximum_distance=all&models[]=&page=' + str(i) + '&stock_type=cpo&zip='
-
+    # website = 'https://www.cars.com/shopping/results/?makes[]=mercedes_benz&maximum_distance=all&models[]=&page=' + str(i) + '&stock_type=cpo&zip='
+    website = 'https://www.cars.com/shopping/results/?dealer_id=&keyword=&list_price_max=&list_price_min=&maximum_distance=20&mileage_max=&monthly_payment=&page=' + str(i) + '&page_size=20&sort=best_match_desc&stock_type=cpo&year_max=&year_min=&zip='
     # Request to website
     response = requests.get(website)
 
@@ -174,7 +53,7 @@ for i in range(1,11):
         try:
             mileage.append(result.find('div', {'class': 'mileage'}).get_text())
         except:
-            mileage.append('n/a')
+            mileage.append('0')
 
         # dealer
         try:
@@ -186,13 +65,13 @@ for i in range(1,11):
         try:
             rating.append(result.find('span', {'class': 'sds-rating__count'}).get_text())
         except:
-            rating.append('n/a')
+            rating.append('0')
 
         # reviews
         try:
             reviews.append(result.find('span', {'class': 'sds-rating__link'}).get_text())
         except:
-            reviews.append('n/a')
+            reviews.append('0')
 
         # price
         try:
@@ -202,11 +81,21 @@ for i in range(1,11):
 
 # Panda Dataframe
 car_dealer = pd.DataFrame({'Name': name, 'Price': price, 'Mileage': mileage, 'Rating': rating, 'Reviews': reviews, 'Dealer': dealer})
+
+car_dealer[['Year', 'Brand', 'Type']] = car_dealer['Name'].apply(extract_info).apply(pd.Series)
+
+# Drop the original 'Name' column if needed
+car_dealer = car_dealer.drop('Name', axis=1)
+
+# Reorder columns for clarity if needed
+car_dealer = car_dealer[['Year', 'Brand', 'Type', 'Price', 'Mileage', 'Rating', 'Reviews', 'Dealer']]
 print(car_dealer)
 
 # Data cleaning
-car_dealer['Reviews'] = car_dealer['Reviews'].apply(lambda x: x.strip('reviews)').strip('('))
-car_dealer['Mileage'] = car_dealer['Mileage'].apply(lambda x: x.strip('mi.'))
+car_dealer['Mileage'] = car_dealer['Mileage'].str.replace(',', '').str.replace(' mi.', '').astype(float)
+car_dealer['Rating'] = car_dealer['Rating'].astype(float)
+car_dealer['Reviews'] = car_dealer['Reviews'].str.replace(',', '').str.replace(' reviews)', '').str.replace(' review)', '').str.replace('(', '').astype(int)
+car_dealer['Price'] = car_dealer['Price'].str.replace(',', '').str.replace('$', '').astype(int) * 100
 
 # Output in Excel
 car_dealer.to_excel('single_page_car.xlsx', index=False)
